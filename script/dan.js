127cc35fc6355a171511ac9ef78a7e236e57c2ec
hyebin <bin930827@gmail.com>
2023-07-28 23:30:40 +0900

    dan99

---
+++ script/dan.js
@@ -0,0 +1,8 @@
+let dan2 = 2;
+const danBtn2 = document.querySelector('.danbtn_2')
+const result = document.querySelector('#result')
+console.log(dan2, danBtn2, result)
+danBtn2.addEventListener('mousedown',function(){
+    result.style.display = 'block';
+    result.innerHTML=`2X1=${dan2}<br>2X2=${dan2 +=2}<br>2X3=${dan2 +=2}<br>2X4=${dan2 +=2}<br>2X5=${dan2 +=2}<br>2X6=${dan2 +=2}<br>2X7=${dan2 +=2}<br>2X8=${dan2 +=2}<br>2X9=${dan2 +=2}`
+})
