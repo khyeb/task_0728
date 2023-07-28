a3d8a94e0a3ce643610bf68390bbba0e1d845867
hyebin <bin930827@gmail.com>
2023-07-28 18:22:25 +0900

    js

---
+++ script/scale.js
@@ -0,0 +1,17 @@
+let userHeight= 160;
+let userWeight= 70;
+let normal_w = 0;
+const heightInput = document.querySelector('#height_input')
+const weightInput = document.querySelector('#weight_input')
+const resultBtn = document.querySelector('#result_btn')
+const resetBtn =document.querySelector('#reset_btn')
+console.log(userHeight, userWeight, normal_w)
+console.log(heightInput, weightInput, resultBtn, resetBtn)
+resultBtn.addEventListener('click',function(){
+    normal_w = (userHeight-100)*0.9
+    window.alert(`적정체중은 ${normal_w}이며 ${userWeight-normal_w}초과되셨습니다.`)
+})
+resetBtn.addEventListener('click', function() {
+    heightInput.value = '';
+    weightInput.value = '';
+})
